ab2858513fa0558238ced9db1ee02d35e8a32b5f
zr1zr <139022173+zr1zr@users.noreply.github.com>
2023-07-09 17:33:27 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,5 @@
 const head = require('./config/head.js');
-const plugins = require('./config/plugins.js');
-const themeConfig = require('./config/themeConfig.js');
+
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
@@ -10,8 +9,27 @@ module.exports = {
   markdown: {
     lineNumbers: true, // 代码行号
   },
+  themeConfig: {
+    sidebar: 'structuring' //  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义
+  },
+  // vssue 评论插件
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "d3ec4ca6363950ca41a2",
+        clientSecret: "897465b6393f1d663e6128d2fab6959a0c0333cc",
+        owner: "zr1zr",
+        repo: "rr-notes",
+      },
+    ],
+  ],
+  
 
   head,
-  plugins,
-  themeConfig,
 }
